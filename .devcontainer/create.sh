ac2e9039b4f7ae23d524f34576b010fb1c605d2e
Nikita Kataev <kaniandr@gmail.com>
2021-05-08 20:43:21 +0300

    [SAPFOR, VSCode, Docker] Add initial configuration for a development container.

---
+++ .devcontainer/create.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git submodule init
+git submodule update
+git submodule foreach git fetch
+git submodule foreach git rebase origin/master
+
+cd extensions
+rm -rf tsar-test && git clone https://github.com/dvm-system/tsar-test
+
+cd ..
+rm -rf build && mkdir build && cd build
+cmake .. -C ../CMakeCache.in -DPTS_EXECUTABLE=/repo/pts/bin/pts.pl
